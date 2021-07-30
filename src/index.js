b67ebb8e19a7aa43252bca44790c69ac9227ca1e
progspectar <progspectar@gmail.com>
2021-07-31 05:04:17 +0700

    feat: delete implementation task

--- src/index.js
+++ src/index.js
@@ -1,130 +1,97 @@
-class Calculator {
-  constructor() {
-    this._symbols = {};
-    this.defineOperator('*', this.multiplication, 'infix', 4);
-    this.defineOperator('/', this.division, 'infix', 4);
-    this.defineOperator('+', this.addition, 'infix', 2);
-    this.defineOperator('-', this.subtraction, 'infix', 2);
-    this.defineOperator('(', this.brackets, 'prefix');
-    this.defineOperator(')', null, 'postfix');
-  }
+// class Calculator {
+//   constructor(expr = '') {
+//     this.tokensString = '()*+-0123456789.';
+//     this.digits = '0123456789.';
+//     this.expr = expr;
 
-  defineOperator(
-    symbol,
-    func,
-    notation = 'func',
-    precedence = 0,
-    rightToLeft = false
-  ) {
-    const numberArgs = notation === 'infix' ? 2 : 1;
-    this._symbols[symbol] = Object.assign({}, this._symbols[symbol], {
-      [notation]: {
-        symbol,
-        func,
-        notation,
-        precedence,
-        rightToLeft,
-        numberArgs: numberArgs,
-      },
-      symbol,
-      regSymbol:
-        symbol.replace(/[\\^$*+?.()|[\]{}]/g, '\\$&') +
-        (/\w$/.test(symbol) ? '\\b' : ''),
-    });
-  }
-  brackets(...a) {
-    return a[a.length - 1];
-  }
+//     this.tokens = {
+//       '+': 'addition',
+//       '-': 'subtraction',
+//       '*': 'multiplication',
+//       '/': 'division',
+//       '(': 'bracketOpen',
+//       ')': 'bracketClosed',
+//       subExpression: 'subExpression',
+//       number: 'number',
+//       unknown: 'unknown',
+//     };
+//     this.currentToken = '';
+//   }
 
-  addition(a, b) {
-    return a + b;
-  }
-  subtraction(a, b) {
-    return a - b;
-  }
-  multiplication(a, b) {
-    return a * b;
-  }
-  division(a, b) {
-    if (b === 0) throw 'TypeError: Division by zero.';
-    return a / b;
-  }
+//   getToken(char) {
+//     if (this.digits.indexOf(char) >= 0) {
+//       return this.tokens.number;
+//     } else if (this.tokens[char]) {
+//       return this.tokens[char];
+//     } else {
+//       return this.tokens.Unknown;
+//     }
+//   }
 
-  calculate(expression) {
-    let match;
-    const values = [],
-      operators = [this._symbols['('].prefix],
-      exec = (_) => {
-        let op = operators.pop();
-        values.push(op.func(...[].concat(...values.splice(-op.numberArgs))));
-        return op.precedence;
-      },
-      noMatchingBracketException = (msg) => msg,
-      pattern = new RegExp(
-        '\\d+(?:\\.\\d+)?|' +
-          Object.values(this._symbols)
-            // longer symbols should be listed first
-            .sort((a, b) => b.symbol.length - a.symbol.length)
-            .map((val) => val.regSymbol)
-            .join('|') +
-          '|(\\S)',
-        'g'
-      );
-    let afterValue = false;
-    pattern.lastIndex = 0;
-    let result = '';
-    do {
-      match = pattern.exec(expression);
-      const [token, bad] = match || [')', undefined],
-        notNumber = this._symbols[token],
-        notNewValue = notNumber && !notNumber.prefix && !notNumber.func,
-        notAfterValue = !notNumber || (!notNumber.postfix && !notNumber.infix);
-      // Check for syntax errors:
-      if (bad || (afterValue ? notAfterValue : notNewValue))
-        return error('Syntax error');
-      if (afterValue) {
-        // We either have an infix or postfix operator (they should be mutually exclusive)
-        const curr = notNumber.postfix || notNumber.infix;
-        do {
-          const prev = operators[operators.length - 1];
-          if ((curr.precedence - prev.precedence || prev.rightToLeft) > 0)
-            break;
-          // Apply previous operator, since it has precedence over current one
-        } while (exec()); // Exit loop after executing an opening bracket
-        afterValue = curr.notation === 'postfix';
-        if (curr.symbol !== ')') {
-          operators.push(curr);
+//   parseTokenized(exprObj, next = '') {
+//     let resArray = [];
+//     const len = exprObj.expr.length;
+//     const expr = exprObj.expr;
+//     let result = 0;
 
-          if (afterValue) exec();
-        }
-      } else if (notNumber) {
-        // prefix operator or function
-        operators.push(notNumber.prefix || notNumber.func);
-        if (notNumber.func) {
-          // Require an opening parenthesis
-          match = pattern.exec(expression);
-          if (!match || match[0] !== '(')
-            return error('Function needs parentheses');
-        }
-      } else {
-        // number
-        values.push(+token);
-        afterValue = true;
-      }
-    } while (match && operators.length);
-    if (operators.length) {
-      throw noMatchingBracketException(
-        'ExpressionError: Brackets must be paired'
-      );
-    } else if (match) {
-      throw noMatchingBracketException(
-        'ExpressionError: Brackets must be paired'
-      );
-    } else {
-      return values.pop();
-    }
-  }
-}
+//     while (
+//       exprObj.curIndex < len - 1 &&
+//       expr[exprObj.curIndex].value !== next
+//     ) {
+//       let curValue = expr[exprObj.curIndex].value;
+//       let curType = expr[exprObj.curIndex].type;
+
+//       if (curValue === '(') {
+//         exprObj.curIndex++;
+//         let subExpr = this.parseTokenized(exprObj, ')');
+//         resArray.push({ type: this.tokens.subExpression, value: subExpr });
+//       } else if (curValue === ')') {
+//         exprObj.curIndex++;
+//       } else {
+//         resArray.push({ type: curType, value: curValue });
+//         exprObj.curIndex++;
+//       }
+//     }
+
+//     return resArray;
+//   }
+
+//   parse(str) {
+//     let prev = { type: '', value: '' };
+//     let curr = { type: '', value: '' };
+//     let value = '';
+//     let res = [];
+
+//     for (const char of str) {
+//       curr = Object.assign({}, { type: this.getToken(char), value: char });
+
+//       if (curr.type === this.tokens.number) {
+//         value += curr.value;
+//         prev = { type: curr.type, value: value };
+//       } else {
+//         if (prev.type === this.tokens.number) {
+//           res.push({ type: prev.type, value: prev.value });
+//         }
+//         res.push(curr);
+//         value = '';
+//         prev = Object.assign({}, curr.type, curr.value);
+//       }
+//     }
+//     if (prev.type === this.tokens.number) {
+//       res.push({ type: prev.type, value: prev.value });
+//     }
+
+//     let exprObj = { curIndex: 0, expr: res };
+//     const res1 = this.parseTokenized(exprObj);
+//     console.log('ggg', JSON.stringify(res1));
+//   }
+
+//   eval() {
+//     const parsed = this.parse(this.expr);
+
+//     return 0;
+//   }
+// }
 
 function eval() {
   // Do not use eval!!!
